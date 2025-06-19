c67f3fb2cbbadca1852104a48c115cbdd1bfcb45
guialmeidajose <guialmeidajose@gmail.com>
2025-06-19 19:52:45 -0300

    inicializacao do algoritmo de busca para os fantasmas (caso o algoritmo usado seja o de busca A*)

--- src/PacMan/src/modelo/Fantasma.java
+++ src/PacMan/src/modelo/Fantasma.java
@@ -10,7 +10,8 @@ import main.PainelJogo;
 
 public class Fantasma extends Entidade{
 
-    private estadoPerseguicao;
+    private int estadoPerseguicao; //1 se o fantasma estiver perseguindo o pacman e 0 caso esteja no modo dispersando
+    private int estadoexecucao; //1 se a sua execucao ja esta em andamento(perseguicao ou dispersao) e 0 caso contrario
     private BufferedImage provisoria;
 
     public Fantasma(PainelJogo painel) {
@@ -33,6 +34,35 @@ public class Fantasma extends Entidade{
     public void desenhar(Graphics2D caneta) {
         BufferedImage imagem = provisoria;
         caneta.drawImage(imagem, getX()  - (getPainelJogo().getTamanhoTile())/2, getY() - (getPainelJogo().getTamanhoTile())/2, getPainelJogo().getTamanhoTile(), getPainelJogo().getTamanhoTile(), null);
+    }
+
+    public int calculaDistancia(int x1, int y1, int x2, int y2){
+        int dist;
+        if(x2 > x1){
+            if(y2 > y1)
+                dist = (x2 - x1) + (y2 - y1);
+            else
+                dist = (x2 - x1) + (y1 - y2);
+        }
+        else{
+            if(y2 > y1)
+                dist = (x1 - x2) + (y2 - y1);
+            else
+                dist = (x1 - x2) + (y1 - y2);
+        }
+        return dist;
+    }
+
+    public void dispersar(int xf, int yf){
+
+    }
+
+    public void perseguir(){
+
+    }
+
+    public void executarfuncao(){
+
     }
 
 
