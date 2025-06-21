a792f43419c97c6b18a54718630e01b882a8521b
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-06-21 10:57:44 -0300

    lembrete pra minha pessoa

--- src/PacMan/src/main/TratadorMapa.java
+++ src/PacMan/src/main/TratadorMapa.java
@@ -94,6 +94,7 @@ public class TratadorMapa {
         int largura, larguraUltima = 0;
         boolean comecouTunelHorizontal;
         boolean[] comecouTuneis = new boolean[maxLargura];
+        boolean[] temFantasmas = new boolean[4];
 
         System.out.println("Checando mapa...");
         try (Scanner scan = new Scanner(new FileReader(getMapaArquivo()))) {
@@ -137,6 +138,8 @@ public class TratadorMapa {
                         comecouTuneis[i] = true;
                     if ((!scan.hasNextLine() && comecouTuneis[i] && linha.charAt(i) != '<') || (!scan.hasNextLine() && !comecouTuneis[i] && linha.charAt(i) == '<'))
                         throw new ArquivoCorrompidoException(String.format("TÚNEL COMEÇADO MAS NÃO ACABADO NA COLUNA %d", i + 1));
+
+                    // IMPLEMENTAR CHEQUE PARA MÚLTIPLOS FANTASMAS DO MESMO TIPO 
                 }
                 larguraUltima = largura;
                 altura++;
