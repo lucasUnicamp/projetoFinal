c091d232d118f4db1eea0de5d80c32c238b2f830
João Pedro Frare <281260@dac.unicamp.br>
2025-06-27 22:41:11 -0300

    imports para as coisas no painelJogo funcionarem

--- src/PacMan/src/main/PainelJogo.java
+++ src/PacMan/src/main/PainelJogo.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
