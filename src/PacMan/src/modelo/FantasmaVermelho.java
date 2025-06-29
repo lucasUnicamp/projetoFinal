0136272c2f2dd0e633069e399ed4cb8b999c598a
Leonardo Ferreira <leoferreira04@outlook.com>
2025-06-29 09:48:19 -0300

    Correção de fantasma vermelho

--- src/PacMan/src/modelo/FantasmaVermelho.java
+++ src/PacMan/src/modelo/FantasmaVermelho.java
@@ -69,40 +69,41 @@ public final class FantasmaVermelho extends Fantasma{
     public void executarfuncao(){
         if(getEstadoPerseguicao() == EstadoPerseguicao.MORTO) {
             funcaoRetorno();
-        }
-        //coordenadas do pac man
-        int x = getPainelJogo().getPacMan().getX();
-        int y = getPainelJogo().getPacMan().getY();
-        //coordenadas do fantasma na matriz
-        int xf = getX()/getPainelJogo().getTamanhoTile();
-        int yf = getY()/getPainelJogo().getTamanhoTile();
-        //coordenadas do destino na matriz
-        int xm = x/getPainelJogo().getTamanhoTile();
-        int ym = y/getPainelJogo().getTamanhoTile();
+        } else {
+            //coordenadas do pac man
+            int x = getPainelJogo().getPacMan().getX();
+            int y = getPainelJogo().getPacMan().getY();
+            //coordenadas do fantasma na matriz
+            int xf = getX()/getPainelJogo().getTamanhoTile();
+            int yf = getY()/getPainelJogo().getTamanhoTile();
+            //coordenadas do destino na matriz
+            int xm = x/getPainelJogo().getTamanhoTile();
+            int ym = y/getPainelJogo().getTamanhoTile();
 
 
-        //perseguir(x, y): caso o fantasma e seu destino nao estejam no mesmo ponto da matriz
-        if(xf != xm || yf != ym){
-            if(getCorrecoesPendentes() > 0){
-                if(getDirecao().equals("direita"))
-                    setX(getX() + getVelocidade());
-                else  if(getDirecao().equals("esquerda"))
-                    setX(getX() - getVelocidade());
-                else if(getDirecao().equals("cima"))
-                    setY(getY() - getVelocidade());
-                else if(getDirecao().equals("baixo"))
-                    setY(getY() + getVelocidade());
-                setCorrecoesPendentes(getCorrecoesPendentes() - getVelocidade());
-                if(getCorrecoesPendentes() <= 0){
-                    setX(xf * getPainelJogo().getTamanhoTile() + getPainelJogo().getTamanhoTile()/2);
-                    setY(yf * getPainelJogo().getTamanhoTile() + getPainelJogo().getTamanhoTile()/2);  
+            //perseguir(x, y): caso o fantasma e seu destino nao estejam no mesmo ponto da matriz
+            if(xf != xm || yf != ym){
+                if(getCorrecoesPendentes() > 0){
+                    if(getDirecao().equals("direita"))
+                        setX(getX() + getVelocidade());
+                    else  if(getDirecao().equals("esquerda"))
+                        setX(getX() - getVelocidade());
+                    else if(getDirecao().equals("cima"))
+                        setY(getY() - getVelocidade());
+                    else if(getDirecao().equals("baixo"))
+                        setY(getY() + getVelocidade());
+                    setCorrecoesPendentes(getCorrecoesPendentes() - getVelocidade());
+                    if(getCorrecoesPendentes() <= 0){
+                        setX(xf * getPainelJogo().getTamanhoTile() + getPainelJogo().getTamanhoTile()/2);
+                        setY(yf * getPainelJogo().getTamanhoTile() + getPainelJogo().getTamanhoTile()/2);  
+                    }
+                    return;
                 }
-                return;
-            }
-            if(getMetaCaminho() == 0)
-                menorCaminho(xm, ym);
-            buscarPonto();
-        }  
+                if(getMetaCaminho() == 0)
+                    menorCaminho(xm, ym);
+                buscarPonto();
+            }  
+        }
     }
 
 }
