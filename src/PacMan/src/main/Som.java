19306161881efbfa9c802a0af1a598788fd5abb2
Leonardo Ferreira <leoferreira04@outlook.com>
2025-06-29 08:02:30 -0300

    Adição de classe para controle de som

---
+++ src/PacMan/src/main/Som.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.IO;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Som {
+    Clip clip;
+    String sons[] = new String[5];
+    public Som() {
+        sons[0] = Paths.get("resources", "sons", "musica.wav").toString();
+    }
+
+    public void carregarArquivo(int i) {
+        try {
+            AudioInputStream stream = AudioSystem.getAudioInputStream(new FileInputStream(sons[i]));
+            clip = AudioSystem.getClip();
+            clip.open(stream);
+        } catch (LineUnavailableException e){
+            System.err.println("Erro ao carregar som.");
+        } catch (IOException e) {
+            System.err.println("Erro ao carregar som.");
+        } catch (UnsupportedAudioFileException e) {
+            System.err.println("Erro ao carregar som.");
+        }
+    }
+
+    public void tocarEfeito(int i) {
+        carregarArquivo(i);
+        clip.start();
+    }
+
+    public void tocarMusica(int i) {
+        carregarArquivo(i);
+        clip.start();
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    } 
+
+    public void parar() {
+        clip.stop();
+    }
+}
